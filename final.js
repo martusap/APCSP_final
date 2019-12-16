fb401c0042abdd5f1bb966a8bf8c80cd4e4b168b
April Matus <apmartus8@gmail.com>
2019-12-16 10:21:44 -0700

    Made edits

--- final.js
+++ final.js
@@ -1,8 +1,8 @@
 
 function ready() {
   go.onclick = function() {
-    let a = course1.value 
-    let b = tiempo1.value
+    let a = course.value 
+    // let b = tiempo1.value
     // let e = course2.value
     // let f = tiempo2.value
     // let g = course3.value
@@ -10,21 +10,21 @@ function ready() {
     // let i = course4.value
     // let j = tiempo4.value
 
-    // var elem = document.getElementById('demo');
-    // elem.parentNode.removeChild(elem);
-
     document.getElementById("demo").style.visibility = "hidden";
 
     clase.innerHTML = a;
 
     let x = 1;
     setTimeout(function(){
-      let c = document.getElementById("demo" + x)
-      let d = tiempo2.value
-      clase.innerHTML = c;
+      let a = document.getElementById("course1").value
+      console.log(a)
+      // let b = tiempo2.value
+      clase.innerHTML = a;
       x = x +1;
     }, 1000);
 
+    clase.innerHTML = a;
+   
   
   }
 
@@ -32,3 +32,65 @@ function ready() {
 }
 
 document.addEventListener("DOMContentLoaded", ready);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <body>
+//     <div>Registration closes in <span id="time">05:00</span> minutes!</div>
+// </body>
+
+// function startTimer(duration, display) {
+//     var timer = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(timer / 60, 10);
+//         seconds = parseInt(timer % 60, 10);
+
+//         minutes = minutes < 10 ? "0" + minutes : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = minutes + ":" + seconds;
+
+//         if (--timer < 0) {
+//             timer = duration;
+//         }
+//     }, 1000);
+// }
+
+// window.onload = function () {
+//     var fiveMinutes = 60 * 5,
+//         display = document.querySelector('#time');
+//     startTimer(fiveMinutes, display);
+// };
