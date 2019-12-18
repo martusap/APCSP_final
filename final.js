af2d844ab3ce18b7c1bda24d1b88e122167901c9
April Matus <apmartus8@gmail.com>
2019-12-18 08:58:53 -0700

    last edits

--- final.js
+++ final.js
@@ -1,96 +1,85 @@
-
 function ready() {
-  go.onclick = function() {
-    let a = course.value 
-    // let b = tiempo1.value
-    // let e = course2.value
-    // let f = tiempo2.value
-    // let g = course3.value
-    // let h = tiempo3.value
-    // let i = course4.value
-    // let j = tiempo4.value
-
+    // Code starts when the button is pressed and form is filled out
+go.onclick = function() {
+    // gets the input from the first class and time on the form
+    let a = course.value;
+    let b = tiempo.value;
+    //   hides the form
     document.getElementById("demo").style.visibility = "hidden";
+    // Places the variables on the screen
+    clase.innerHTML = a;  
+    tie.innerHTML = b;
+    // should set the countdown timer
+    var myVar = setInterval(myTimer, 1000);
 
-    clase.innerHTML = a;
+    function myTimer() {
+      if (b>0) {
+        tie.innerHTML = b;
+        b = b - 1;
+      }
+      else {
+    // If b is 0 then it will set up the next homework assignment
+        a = document.getElementById("course1").value
+        b = document.getElementById("tiempo1").value
+        clase.innerHTML = a;
+        tie.innerHTML = b;
+        // Tried to clear interval so it doesnt take 2 off of b everytime
+        clearInterval(myVar);
+        var myVarw = setInterval(nextTimer, 1000);
+      }
+    }
 
-    let x = 1;
-    setTimeout(function(){
-      let a = document.getElementById("course1").value
-      console.log(a)
-      // let b = tiempo2.value
-      clase.innerHTML = a;
-      x = x +1;
-    }, 1000);
+    function nextTimer() {
+      // b = document.getElementById("tiempo1").value
+      if (b>0) {
+        tie.innerHTML = b;
+        b = b - 1;
+      }
+      else {
+        a = document.getElementById("cource2").value
+        b = document.getElementById("tiempo2").value
+        clase.innerHTML = a;
+        tie.innerHTML = b;
+        clearInterval(Myvarw);
+        var myVara = setInterval(threeTimer, 1000);
+      }
+    }
 
-    clase.innerHTML = a;
-   
-  
-  }
+    function threeTimer() {
+      if (b>0) {
+        tie.innerHTML = b;
+        b = b - 1;
+      }
+      else {
+        a = document.getElementById("course3").value
+        b = document.getElementById("tiempo3").value
+        clase.innerHTML = a;
+        tie.innerHTML = b;
+         clearInterval(myVara);
+        var myVard = setInterval(fourTimer, 1000);
+      }
+    }
 
+    function fourTimer() {
+      if (b>0) {
+        tie.innerHTML = b;
+        b = b - 1;
+      }
+      else {
+         clearInterval(myVard);
+        // a = document.getElementById("course3").value
+        // b = document.getElementById("tiempo3").value
+        // clase.innerHTML = a;
+        // tie.innerHTML = b;
+        // setInterval(fourTimer, 1000);
+        clase.innerHTML = "Congradulations you have finished your homework, take the rest of the night off";
+        tie.innerHTML = "";
 
+      }
+    }
+
+        // Now done with homework, a little congradulations message
 }
-
-document.addEventListener("DOMContentLoaded", ready);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// <body>
-//     <div>Registration closes in <span id="time">05:00</span> minutes!</div>
-// </body>
-
-// function startTimer(duration, display) {
-//     var timer = duration, minutes, seconds;
-//     setInterval(function () {
-//         minutes = parseInt(timer / 60, 10);
-//         seconds = parseInt(timer % 60, 10);
-
-//         minutes = minutes < 10 ? "0" + minutes : minutes;
-//         seconds = seconds < 10 ? "0" + seconds : seconds;
-
-//         display.textContent = minutes + ":" + seconds;
-
-//         if (--timer < 0) {
-//             timer = duration;
-//         }
-//     }, 1000);
-// }
-
-// window.onload = function () {
-//     var fiveMinutes = 60 * 5,
-//         display = document.querySelector('#time');
-//     startTimer(fiveMinutes, display);
-// };
+}
+    
+   document.addEventListener("DOMContentLoaded", ready);    
